8ee27385b9e549cab25c05fa7dfe0c7107c27221
Becky Phan <beckyphan@live.com>
2019-08-02 20:35:01 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,11 +1,17 @@
 # Create your costume_stores migration here
 
+class CostumeStores < ActiveRecord::Migration[4.2]
+  
   def change 
-    create_table :costumes do |t|
+    create_table :costume_stores do |t|
       t.string :name
-      t.float :price
-      t.integer :size 
-      t.string :image_url
+      t.string :location
+      t.integer :inventory
+      t.integer :employees
+      t.boolean :status
+      t.time
       t.timestamps
     end
   end 
+  
+end 
