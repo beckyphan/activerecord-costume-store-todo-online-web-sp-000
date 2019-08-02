424c0b973cc8f9c8c20f0260aa3926671900fba1
Becky Phan <beckyphan@live.com>
2019-08-02 20:36:15 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
 # Create your haunted_houses migration here
+
+class HauntedHouse < ActiveRecord::Migration[4.2]
+  
+  def change 
+    create_table :haunted_house do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :type
+      t.time :open
+      t.time :close
+    end
+  end 
+  
+end 
