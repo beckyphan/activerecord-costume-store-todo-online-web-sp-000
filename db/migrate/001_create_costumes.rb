7ceeeace5bfa7ac8b1e13185f9809c8f7fe134b9
Becky Phan <beckyphan@live.com>
2019-08-02 20:32:57 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,7 +11,14 @@
 class Costumes < ActiveRecord::Migration[4.2]
   
   def change 
-    create_table 
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.integer :size 
+      t.string :image_url
+      t.timestamp :created_at
+      t.timestamp :updated_at
+    end
   end 
   
 end 
