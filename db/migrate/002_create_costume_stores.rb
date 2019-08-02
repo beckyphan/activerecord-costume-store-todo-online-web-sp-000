64ebfa5ea94a5ae13a8daef94ab06005b160b998
Becky Phan <beckyphan@live.com>
2019-08-02 20:44:08 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :inventory
+      t.integer :costume_inventory
       t.integer :employees
       t.binary :in_business
       t.time :open
