267357979cae137c522b872943fef4b804b1b1c3
Becky Phan <beckyphan@live.com>
2019-08-02 20:49:20 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,8 +9,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
-      t.date :open
-      t.date :close
+      t.date :opening_date
+      t.date :closing_date
       t.text :description
     end
   end 
